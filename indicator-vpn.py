b2b4bccd10439ec7d3b6b54c4a9f0ad36cd11920
ludovic geoffroy <ludovic.geoffroy@thetribe.io>
2020-02-05 12:30:49 +0100

    add double vpn and p2p connection options

--- indicator-vpn.py
+++ indicator-vpn.py
@@ -45,6 +45,9 @@ class IndicatorVpn:
         self.disconnect_menu.connect('activate', self.disconnect)
         self.menu.append(self.disconnect_menu)
 
+        item = Gtk.SeparatorMenuItem()
+        self.menu.append(item)
+
         countries_menu = Gtk.Menu()
         countries = [menu for menu in
                         filter(None, re.split('\t|\n| |-|\\|/|\|', self.transform_output(subprocess.run(['nordvpn', 'countries'], stdout=subprocess.PIPE))))]
@@ -56,9 +59,43 @@ class IndicatorVpn:
             countries_menu.append(item)
 
         self.change_server_menu = Gtk.MenuItem()
-        self.change_server_menu.set_label('Choose server...')
+        self.change_server_menu.set_label('Choose server country...')
         self.change_server_menu.set_submenu(countries_menu)
         self.menu.append(self.change_server_menu)
+
+        p2p_menu = Gtk.Menu()
+
+        fr_p2p = Gtk.MenuItem()
+        fr_p2p.set_label('France')
+        fr_p2p.connect('activate', self.connect_p2p, True)
+        p2p_menu.append(fr_p2p)
+
+        lu_p2p = Gtk.MenuItem()
+        lu_p2p.set_label('Luxembourg')
+        lu_p2p.connect('activate', self.connect_p2p, False)
+        p2p_menu.append(lu_p2p)
+
+        self.change_p2p_menu = Gtk.MenuItem()
+        self.change_p2p_menu.set_label('Select P2P server...')
+        self.change_p2p_menu.set_submenu(p2p_menu)
+        self.menu.append(self.change_p2p_menu)
+
+        double_menu = Gtk.Menu()
+
+        fr_double = Gtk.MenuItem()
+        fr_double.set_label('France')
+        fr_double.connect('activate', self.connect_double, True)
+        double_menu.append(fr_double)
+
+        nl_double = Gtk.MenuItem()
+        nl_double.set_label('Netherlands')
+        nl_double.connect('activate', self.connect_double, False)
+        double_menu.append(nl_double)
+
+        self.change_double_menu = Gtk.MenuItem()
+        self.change_double_menu.set_label('Select DoubleVPN server...')
+        self.change_double_menu.set_submenu(double_menu)
+        self.menu.append(self.change_double_menu)
 
         item = Gtk.SeparatorMenuItem()
         self.menu.append(item)
@@ -93,7 +130,7 @@ class IndicatorVpn:
             self.set_icon('connected.png' if state == 'Connected' else 'disconnected.png')
             self.connect_menu.set_sensitive(False if state == 'Connected' else True)
             self.disconnect_menu.set_sensitive(True if state == 'Connected' else False)
-            self.change_server_menu.set_label('Change server...' if state == 'Connected' else 'Choose server...')
+            self.change_server_menu.set_label('Change server country...' if state == 'Connected' else 'Choose server country...')
             self.state = state
         return True
 
@@ -109,12 +146,20 @@ class IndicatorVpn:
         self.send_notification(text)
         self.set_icon('disconnected.png')
 
-    def change_server(self, widget, country):
+    def change_server(self, widget, country_or_server):
         with open(os.devnull, 'w') as devnull:
             subprocess.run(['nordvpn', 'disconnect'], stdout=devnull)
-        result = subprocess.run(['nordvpn', 'connect', country], stdout=subprocess.PIPE)
+        result = subprocess.run(['nordvpn', 'connect', country_or_server], stdout=subprocess.PIPE)
         text = self.transform_output(result).split('\n')[1]
         self.send_notification(text)
+
+    def connect_p2p(self, widget, french_server):
+        server = 'fr273' if french_server else 'lu79'
+        self.change_server(widget, server)
+
+    def connect_double(self, widget, french_server):
+        server = 'uk-fr4' if french_server else 'ch-nl3'
+        self.change_server(widget, server)
 
     def quit(self, widget):
         Gtk.main_quit()
